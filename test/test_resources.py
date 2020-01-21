e8d8d7feb5798f9cba1ab1517a4efabc0fe04146
Geoambiente <gisti@geoambiente.com.br>
2020-01-21 10:39:05 -0300

    Pasta teste

---
+++ test/test_resources.py
@@ -0,0 +1,46 @@
+# coding=utf-8
+"""Resources test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+__author__ = 'gisti@geoambiente.com.br'
+__date__ = '2018-09-06'
+__copyright__ = 'Copyright 2018, Geoambiente'
+
+import unittest
+from test.utilities import get_qgis_app
+
+QGIS_APP = get_qgis_app()
+
+from PyQt4.QtGui import QIcon
+
+
+class CQFSResourcesTest(unittest.TestCase):
+    """Test rerources work."""
+
+    def setUp(self):
+        """Runs before each test."""
+        pass
+
+    def tearDown(self):
+        """Runs after each test."""
+        pass
+
+    def test_icon_png(self):
+        """Test we can click OK."""
+        path = ':/plugins/CQFS/icon.png'
+        icon = QIcon(path)
+        self.assertFalse(icon.isNull())
+
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(CQFSResourcesTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+
+
